903428a02b7347aaa1011f019237ac57d722cc6d
ShowBizTorize <beamjames336@gmail.com>
2024-10-10 17:52:56 -0700

    bhyudef7832irfehfuieywdu

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Vector2 direction = Vector2.right;
+    float speed = 4.0f;
+    float time = 0.0f;
+
+    public GameObject Player1;
+    public GameObject Player2;
+    private float NoBounce=0f;
+    void Start()
+    {
+ 
+        float angle = 30.0f * Mathf.Deg2Rad;
+        direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        transform.position = Vector3.zero;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NoBounce>0)
+        NoBounce-=Time.deltaTime;
+        else{
+        if (transform.position.x > 7.5f)
+        {
+            direction.x = -direction.x;
+        }
+         if (transform.position.y > 3.5f)
+        {
+            speed = speed*1.02f;
+            direction.y = -direction.y;
+        }
+        if (transform.position.x < -7.5f)
+        {
+            direction.x = -direction.x;
+        }
+         if (transform.position.y < -3.5f)
+        {
+            direction.y = -direction.y;
+        }
+        if (IsBounded(Player1)||IsBounded(Player2)){
+            direction.x = -direction.x;
+        }
+        NoBounce=0.03f;
+        }
+        float dt = Time.deltaTime;
+        Vector3 change = direction * speed * dt;
+        transform.position += change;
+        
+        
+    }
+/// <summary>
+/// Checks to see if the ball is "touching the paddle"
+/// </summary>
+/// <param name="Paddle">The paddle object</param>
+/// <returns></returns>
+    bool IsBounded(GameObject Paddle)
+    {
+        Vector3 Average = (transform.position+Paddle.GetComponent<Transform>().transform.position)/2;
+  
+           Vector3 BallAverage = new Vector3(Mathf.Clamp(Average.x,transform.position.x-transform.localScale.x/2,transform.position.x+transform.localScale.x/2),
+        Mathf.Clamp(Average.y,transform.position.y-transform.localScale.y/2,transform.position.y+transform.localScale.y/2),
+        Mathf.Clamp(Average.z,transform.position.z-transform.localScale.z/2,transform.position.z+transform.localScale.z/2));
+
+        Vector3 PaddleAverage = new Vector3(Mathf.Clamp(Average.x,Paddle.GetComponent<Transform>().transform.position.x-Paddle.GetComponent<Transform>().transform.localScale.x/2,Paddle.GetComponent<Transform>().transform.position.x+Paddle.GetComponent<Transform>().transform.localScale.x/2),
+        Mathf.Clamp(Average.y,Paddle.GetComponent<Transform>().transform.position.y-Paddle.GetComponent<Transform>().transform.localScale.y/2,Paddle.GetComponent<Transform>().transform.position.y+Paddle.GetComponent<Transform>().transform.localScale.y/2),
+        Mathf.Clamp(Average.z,Paddle.GetComponent<Transform>().transform.position.z-Paddle.GetComponent<Transform>().transform.localScale.z/2,Paddle.GetComponent<Transform>().transform.position.z+Paddle.GetComponent<Transform>().transform.localScale.z/2));
+
+        Debug.DrawLine(BallAverage,transform.position,Color.red,2,false);
+        Debug.DrawLine(PaddleAverage,Paddle.GetComponent<Transform>().transform.position,Color.red,2,false);
+        return PaddleAverage==BallAverage;
+    }
+
+}
---
+++ Assets/Scripts/PlayerPaddle.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Paddle : MonoBehaviour
+{
+    public bool Player2=false;
+    private float FieldHeight = 2.93f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    Vector3 up = Vector3.up;
+    Vector3 down = Vector3.down;
+
+
+    Vector3 down1 = transform.position;
+    Vector3 down2 = down1 + down * 5.0f;
+
+    Vector3 up1 = transform.position;
+    Vector3 up2 = up1 + up * 5.0f;
+
+
+    float dt = Time.deltaTime;
+    float speed = 23.0f;
+
+    if ((Input.GetKey(KeyCode.W)&&!Player2)||(Input.GetKey(KeyCode.UpArrow)&&Player2))
+    transform.position += Vector3.up * speed * Time.deltaTime;
+    
+    if ((Input.GetKey(KeyCode.S)&&!Player2)||(Input.GetKey(KeyCode.DownArrow)&&Player2))
+    transform.position += Vector3.down * speed * Time.deltaTime;
+
+     transform.position = new Vector3(transform.position.x,Mathf.Clamp(transform.position.y,-FieldHeight,FieldHeight),transform.position.z);
+    }
+    
+   
+}
