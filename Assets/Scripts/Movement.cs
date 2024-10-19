98c643343684f5fee298aca4bf78310aa2e64802
ShowBizTorize <beamjames336@gmail.com>
2024-10-18 17:17:05 -0700

    Finishing Touches

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,22 +9,36 @@ public class Ball : MonoBehaviour
     public GameObject Player1;
     public GameObject Player2;
     private float NoBounce=0f;
+    
+    int Player_Score_1 = 0;
+    int Player_Score_2 = 0;
+    int Scoring_Point = 1;
     void Start()
     {
  
-        float angle = 30.0f * Mathf.Deg2Rad;
+        float angle = 38.0f * Mathf.Deg2Rad;
         direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
         transform.position = Vector3.zero;
+
+        Debug.Log("Start!");
+        Debug.Log( Player_Score_1 + " = " + Player_Score_2 );
     }
 
 
     // Update is called once per frame
     void Update()
     {
+
+        if (transform.position.x != Mathf.Clamp(transform.position.x,-9f,9f) )
+        {
+            transform.position = new Vector2(0,0);
+            speed = 4.0f;
+        }
+
         if (NoBounce>0)
         NoBounce-=Time.deltaTime;
         else{
-        if (transform.position.x > 7.5f)
+        if (transform.position.x > 9.1f) // Origionaly 7.5
         {
             direction.x = -direction.x;
         }
@@ -33,12 +47,13 @@ public class Ball : MonoBehaviour
             speed = speed*1.02f;
             direction.y = -direction.y;
         }
-        if (transform.position.x < -7.5f)
+        if (transform.position.x < -9.1f)
         {
             direction.x = -direction.x;
         }
          if (transform.position.y < -3.5f)
         {
+            speed = speed*1.02f;
             direction.y = -direction.y;
         }
         if (IsBounded(Player1)||IsBounded(Player2)){
@@ -49,14 +64,13 @@ public class Ball : MonoBehaviour
         float dt = Time.deltaTime;
         Vector3 change = direction * speed * dt;
         transform.position += change;
-        
-        
-    }
-/// <summary>
-/// Checks to see if the ball is "touching the paddle"
-/// </summary>
-/// <param name="Paddle">The paddle object</param>
-/// <returns></returns>
+
+
+    /// <summary>
+    /// Checks to see if the ball is "touching the paddle"
+    /// </summary>
+    /// <param name="Paddle">The paddle object</param>
+    /// <returns></returns>
     bool IsBounded(GameObject Paddle)
     {
         Vector3 Average = (transform.position+Paddle.GetComponent<Transform>().transform.position)/2;
@@ -71,4 +85,17 @@ public class Ball : MonoBehaviour
         return PaddleAverage==BallAverage;
     }
 
-}
+        if (transform.position.x > 9f)
+    {//The ball gets reset from the right 
+        Player_Score_1++;
+        Debug.Log( Player_Score_1 + " = " + Player_Score_2 );
+    }
+
+            if (transform.position.x < -9f)
+    {//The ball gets reset from the right 
+        Player_Score_2++;
+        Debug.Log( Player_Score_1 + " = " + Player_Score_2 );
+    }
+
+
+}}
